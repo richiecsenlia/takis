3f95808cbc045921b195f47ca30f29d0fa45c2b2
ezrapasha <ezra.pasha@ui.ac.id>
2023-03-07 13:33:06 +0700

    [GREEN] implemented LogTA choices in form_log_TA view

--- pengisianLog/views.py
+++ pengisianLog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import LogTA
 
 # Create your views here.
 def form_log_TA(request):
-    return render(request, 'form_log.html', {})
+    return render(request, 'form_log.html', {'kategori_choice': LogTA.kategori.field.choices, 
+        'periode_choice': LogTA.periode.field.choices, 
+        'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
