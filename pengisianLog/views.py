980f72e01e8979da1eb99266594293806f4db2db
ezrapasha <ezra.pasha@ui.ac.id>
2023-03-13 09:50:40 +0700

    [GREEN] Implemented POST method for LogTA view

--- pengisianLog/views.py
+++ pengisianLog/views.py
@@ -4,6 +4,23 @@ from .models import LogTA
 
 # Create your views here.
 def form_log_TA(request):
-    return render(request, 'form_log.html', {'kategori_choice': LogTA.kategori.field.choices, 
-        'periode_choice': LogTA.periode.field.choices, 
-        'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
+    if request.method == 'GET':
+        return render(request, 'form_log.html', {'kategori_choice': LogTA.kategori.field.choices, 
+            'periode_choice': LogTA.periode.field.choices, 
+            'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
+    else:
+        LogTA.objects.create(
+            kategori = request.POST.get('kategori'),
+            jenis_pekerjaan = request.POST.get('pekerjaan'),
+            detail_kegiatan = request.POST.get('detail_kegiatan'),
+            pemberi_tugas = request.POST.get('pemberi_tugas'),
+            uraian = request.POST.get('uraian'),
+            periode = request.POST.get('periode'),
+            bulan_pengerjaan = request.POST.get('bulan_pengerjaan'),
+            jumlah_rencana_kinerja = int(request.POST.get('jumlah_kinerja')),
+            satuan_rencana_kinerja = request.POST.get('satuan_kinerja'),
+            konversi_jam_rencana_kinerja = int(request.POST.get('jam_rencana_kinerja'))
+        )
+        return render(request, 'form_log.html', {'kategori_choice': LogTA.kategori.field.choices, 
+            'periode_choice': LogTA.periode.field.choices, 
+            'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
