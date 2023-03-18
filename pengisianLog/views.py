f4c24c34a936b97e9e46fd74d3233c412b31ed52
ezrapasha <ezra.pasha@ui.ac.id>
2023-03-18 23:02:27 +0700

    [GREEN] Implemented handling for ValueError in LogTA view

--- pengisianLog/views.py
+++ pengisianLog/views.py
@@ -9,25 +9,30 @@ from .models import LogTA
 # Create your views here.
 @ta_required
 def form_log_TA(request):
-    if request.method == 'GET':
+    try:
+        if request.method == 'GET':
+            return render(request, 'form_log.html', {'kategori_choice': LogTA.kategori.field.choices, 
+                'periode_choice': LogTA.periode.field.choices, 
+                'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
+        else:
+            LogTA.objects.create(
+                user = request.user,
+                kategori = request.POST.get('kategori'),
+                jenis_pekerjaan = request.POST.get('pekerjaan'),
+                detail_kegiatan = request.POST.get('detail_kegiatan'),
+                pemberi_tugas = request.POST.get('pemberi_tugas'),
+                uraian = request.POST.get('uraian'),
+                periode = request.POST.get('periode'),
+                bulan_pengerjaan = request.POST.get('bulan_pengerjaan'),
+                jumlah_rencana_kinerja = int(request.POST.get('jumlah_kinerja')),
+                satuan_rencana_kinerja = request.POST.get('satuan_kinerja'),
+                konversi_jam_rencana_kinerja = int(request.POST.get('jam_rencana_kinerja'))
+            )
+            return redirect(reverse("pengisianLog:daftarLogTA"))
+    except ValueError:
         return render(request, 'form_log.html', {'kategori_choice': LogTA.kategori.field.choices, 
-            'periode_choice': LogTA.periode.field.choices, 
-            'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
-    else:
-        LogTA.objects.create(
-            user = request.user,
-            kategori = request.POST.get('kategori'),
-            jenis_pekerjaan = request.POST.get('pekerjaan'),
-            detail_kegiatan = request.POST.get('detail_kegiatan'),
-            pemberi_tugas = request.POST.get('pemberi_tugas'),
-            uraian = request.POST.get('uraian'),
-            periode = request.POST.get('periode'),
-            bulan_pengerjaan = request.POST.get('bulan_pengerjaan'),
-            jumlah_rencana_kinerja = int(request.POST.get('jumlah_kinerja')),
-            satuan_rencana_kinerja = request.POST.get('satuan_kinerja'),
-            konversi_jam_rencana_kinerja = int(request.POST.get('jam_rencana_kinerja'))
-        )
-        return redirect(reverse("pengisianLog:daftarLogTA"))
+                'periode_choice': LogTA.periode.field.choices, 
+                'bulan_choice': LogTA.bulan_pengerjaan.field.choices})
 
 def daftarLogTA(request):
     logs = LogTA.objects.filter(user=request.user)
