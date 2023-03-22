e9e83a0504425506156c04c2befe20983a7f653b
Richie Christiansen Senlia <richie.christiansen@ui.ac.id>
2023-03-22 21:26:44 +0700

    Revert "Merge branch 'PBI-1-authentication' into 'master'"

--- takis/settings.py
+++ takis/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
+
 from pathlib import Path
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,15 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+#9dgz=0f9#ax1b8v)6!ot^d9aiiwu4@aiap92c!*xrsoiqa55'
 
-
-APP_NAME = os.getenv('APP_NAME', '')
-
-PRODUCTION = os.getenv('DATABASE_URL') is not None
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not PRODUCTION
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,25 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'pengisianLog',
-    'django_cas_ng',
-    'crispy_forms',
-    'crispy_bootstrap5',
-    'authentication',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'authentication.middleware.simple_middleware',
-    #'django_cas_ng.middleware.CASMiddleware',
 ]
 
 ROOT_URLCONF = 'takis.urls'
@@ -68,7 +54,7 @@ ROOT_URLCONF = 'takis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,9 +66,6 @@ TEMPLATES = [
         },
     },
 ]
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'takis.wsgi.application'
 
@@ -96,10 +79,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-if PRODUCTION:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=600, ssl_require=True
-    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -119,15 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'django_cas_ng.backends.CASBackend',
-)
 
-CAS_SERVER_URL = os.getenv("CAS_SERVER_URL", 'https://sso.ui.ac.id/cas2/')
-CAS_LOGIN_URL_NAME = "authentication:login"
-CAS_REDIRECT_URL = "main:homepage"
-CAS_IGNORE_REFERER = True
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -145,22 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# This is the directory for storing `collectstatic` results.
-# This shouldn't be included in your Git repository.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# You can use this directory to store project-wide static files.
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-# Make sure the directories exist to prevent errors when doing `collectstatic`.
-for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
-    directory.mkdir(exist_ok=True)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = [f'http://{APP_NAME}.up.railway.app', f'https://{APP_NAME}.up.railway.app']
