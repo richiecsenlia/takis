cbdbc1ba8ded59f82d7c1369b0aac9692327e39c
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2023-03-24 04:47:33 +0700

    [GREEN] add slug in profile model for url need

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 # Create your models here.
 class MataKuliah(models.Model):
@@ -17,10 +19,10 @@ class TeachingAssistantProfile(models.Model):
                       ('Lulus S2','Lulus S2'),
                       ('Lulus S2 MTI','Lulus S2 MTI')]
 
-    PRODI_CHOICES = [('IK','Ilmu Komputer'),
-                     ('IK KKI','Ilmu Komputer KKI'),
-                     ('SI','Sistem Informasi'),
-                     ('TI','Teknologi Informasi')]
+    PRODI_CHOICES = [('Ilmu Komputer','Ilmu Komputer'),
+                     ('Ilmu Komputer KKI','Ilmu Komputer KKI'),
+                     ('Sistem Informasi','Sistem Informasi'),
+                     ('Teknologi Informasi','Teknologi Informasi')]
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nama = models.CharField(max_length=50, verbose_name='Nama Lengkap')
@@ -28,6 +30,15 @@ class TeachingAssistantProfile(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name='Status Kemahasiswaan')
     prodi = models.CharField(max_length=30, choices=PRODI_CHOICES, verbose_name='Program Studi')
     daftar_matkul = models.ManyToManyField(MataKuliah)
+    slug = models.SlugField(unique=True, verbose_name='URL Slug')
 
     def __str__(self):
         return self.nama
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.nama)
+        return super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('profile', args=[str(self.slug)])
