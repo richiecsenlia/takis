8c65de098e02200a9e5ff7e775ab3315135c4db2
ezrapasha <ezra.pasha@ui.ac.id>
2023-04-06 14:48:38 +0700

    [RED] created test for getting all average method

---
+++ rekapanLog/tests.py
@@ -0,0 +1,73 @@
+from django.test import TestCase
+from django.test import RequestFactory
+from django.contrib.auth.models import User
+from .views import *
+from pengisianLog.models import LogTA
+
+# Create your tests here.
+class RekapanLogTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.ta_user = User.objects.create(username='ta', password='ta', email='ta@ta.com')
+        self.ta_user.role.role = 'TA'
+        self.ta_user.role.save()
+
+        LogTA.objects.create(
+            user = self.ta_user,
+            kategori = "Penyelenggaraan Kuliah",
+            jenis_pekerjaan = "Membuat Soal",
+            detail_kegiatan = "Essay dan Pilgan",
+            pemberi_tugas = "Ibu Ika Alfina",
+            uraian = "Membuat soal PR",
+            periode = "Semester Kuliah",
+            bulan_pengerjaan = "MAR",
+            jumlah_rencana_kinerja = 4,
+            satuan_rencana_kinerja = "Tugas",
+            konversi_jam_rencana_kinerja = 1,
+            jumlah_realisasi_kinerja = 0,
+            satuan_realisasi_kinerja = "",
+            konversi_jam_realisasi_kinerja = 0
+        )
+
+        LogTA.objects.create(
+            user = self.ta_user,
+            kategori = "Penyelenggaraan Kuliah",
+            jenis_pekerjaan = "Koreksi Tugas",
+            detail_kegiatan = "Essay dan Pilgan",
+            pemberi_tugas = "Ibu Ika Alfina",
+            uraian = "Mengoreksi PR",
+            periode = "Semester Kuliah",
+            bulan_pengerjaan = "APR",
+            jumlah_rencana_kinerja = 2,
+            satuan_rencana_kinerja = "Tugas",
+            konversi_jam_rencana_kinerja = 0.5,
+            jumlah_realisasi_kinerja = 0,
+            satuan_realisasi_kinerja = "",
+            konversi_jam_realisasi_kinerja = 0
+        )
+
+        LogTA.objects.create(
+            user = self.ta_user,
+            kategori = "Persiapan Kuliah",
+            jenis_pekerjaan = "Mempersiapkan Environment",
+            detail_kegiatan = "SCELE",
+            pemberi_tugas = "Ibu Putu",
+            uraian = "Rapat persiapan",
+            periode = "Adhoc",
+            bulan_pengerjaan = "SEP",
+            jumlah_rencana_kinerja = 2,
+            satuan_rencana_kinerja = "Tugas",
+            konversi_jam_rencana_kinerja = 2,
+            jumlah_realisasi_kinerja = 0,
+            satuan_realisasi_kinerja = "",
+            konversi_jam_realisasi_kinerja = 0
+        )
+
+    def test_get_average_all_rencana(self):
+        persiapan, penyelenggaraan, dukungan, pengembangan, riset = get_all_rencana()
+        # penyelenggaraan harusnya 0.25, persiapan harusnya 0.33 (rata2)
+
+        self.assertEquals(penyelenggaraan, 0.25)
+        self.assertEquals(persiapan, (1/3))
+
---
+++ rekapanLog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def get_all_rencana():
+    return 0, 0, 0, 0, 0
