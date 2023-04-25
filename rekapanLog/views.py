f13efd7d0dceddade9c2ce85f7ebbd7237d0dac8
ezrapasha <ezra.pasha@ui.ac.id>
2023-04-25 20:33:11 +0700

    [GREEN] Implemented realisasi in get avg method

--- rekapanLog/views.py
+++ rekapanLog/views.py
@@ -10,7 +10,12 @@ def get_all_rencana(user_):
         penyelenggaraan_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Penyelenggaraan Kuliah'))/6,
         dukungan_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Dukungan Kuliah Kakak Asuh'))/6,
         pengembangan_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Pengembangan Institusi'))/6,
-        riset_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Riset dan Pusilkom'))/6) 
+        riset_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Riset dan Pusilkom'))/6,
+        persiapan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Persiapan Kuliah'))/6, 
+        penyelenggaraan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Penyelenggaraan Kuliah'))/6,
+        dukungan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Dukungan Kuliah Kakak Asuh'))/6,
+        pengembangan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Pengembangan Institusi'))/6,
+        riset_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Riset dan Pusilkom'))/6) 
     
     return aggr
 
@@ -19,7 +24,12 @@ def get_month_rencana(user_, month):
         penyelenggaraan_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Penyelenggaraan Kuliah', bulan_pengerjaan=month)),
         dukungan_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Dukungan Kuliah Kakak Asuh', bulan_pengerjaan=month)),
         pengembangan_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Pengembangan Institusi', bulan_pengerjaan=month)),
-        riset_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Riset dan Pusilkom', bulan_pengerjaan=month))) 
+        riset_plan=Sum("konversi_jam_rencana_kinerja", filter=Q(kategori='Riset dan Pusilkom', bulan_pengerjaan=month)),
+        persiapan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Persiapan Kuliah', bulan_pengerjaan=month)), 
+        penyelenggaraan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Penyelenggaraan Kuliah', bulan_pengerjaan=month)),
+        dukungan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Dukungan Kuliah Kakak Asuh', bulan_pengerjaan=month)),
+        pengembangan_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Pengembangan Institusi', bulan_pengerjaan=month)),
+        riset_real=Sum("konversi_jam_realisasi_kinerja", filter=Q(kategori='Riset dan Pusilkom', bulan_pengerjaan=month))) 
     
     return rekapBulan
 
