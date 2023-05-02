379f3da6aa48de65b33084f5fe9adb6a62b8ab6d
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2023-05-03 06:11:35 +0700

    [REFACTOR] update register and change pw forms

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,15 +3,20 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserForm(forms.Form):
-    username = forms.CharField(label='username', min_length=0, max_length=150)
-    password = forms.CharField(label='password', widget=forms.PasswordInput)
 class UserCreateForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan password anda'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan ulang password anda'}))
     class Meta:
         model = User
-        fields = ['username','password1','password2']
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan username anda'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan email anda'}),
+        }
     
 class PasswordForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan password baru anda'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan ulang password baru anda'}))
     class Meta:
         model = User
         fields = ['password1','password2']
