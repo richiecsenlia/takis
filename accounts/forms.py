c07fb581b67a1f7a9bb77b94bdd4b6f03149741d
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2023-05-26 05:51:34 +0700

    [GREEN] implemented edit profile and fill matkul

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,17 +4,34 @@ from accounts.models import MataKuliah, TeachingAssistantProfile
 class TAProfileForm(forms.ModelForm):
     class Meta:
         model = TeachingAssistantProfile
-        fields = ['nama', 'kontrak', 'status', 'prodi', 'daftar_matkul', 'bulan_mulai', 'bulan_selesai']
+        fields = ['nama', 'kontrak', 'status', 'prodi', 'bulan_mulai', 'bulan_selesai', 'daftar_matkul']
 
         daftar_matkul = forms.ModelMultipleChoiceField(
-            queryset=MataKuliah.objects.all(),
-            widget=forms.CheckboxSelectMultiple
+            queryset=MataKuliah.objects.all()
         )
         widgets = {
-            'nama': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Masukkan nama lengkap anda'}),
-            'kontrak': forms.Select(attrs={'class': 'form-select'}),
-            'status': forms.Select(attrs={'class': 'form-select'}),
-            'prodi': forms.Select(attrs={'class': 'form-select'}),
-            'bulan_mulai': forms.Select(attrs={'class': 'form-select'}),
-            'bulan_selesai': forms.Select(attrs={'class': 'form-select'})
+            'nama': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Masukkan nama lengkap anda'}
+            ),
+            'kontrak': forms.Select(
+                attrs={'class': 'form-select'},
+                choices=TeachingAssistantProfile.KONTRAK_CHOICES,
+            ),
+            'status': forms.Select(
+                attrs={'class': 'form-select'},
+                choices=TeachingAssistantProfile.STATUS_CHOICES,
+            ),
+            'prodi': forms.Select(
+                attrs={'class': 'form-select'},
+                choices=TeachingAssistantProfile.PRODI_CHOICES,
+            ),
+            'bulan_mulai': forms.Select(
+                attrs={'class': 'form-select'},
+                choices=TeachingAssistantProfile.BULAN_CHOICES,
+            ),
+            'bulan_selesai': forms.Select(
+                attrs={'class': 'form-select'},
+                choices=TeachingAssistantProfile.BULAN_CHOICES,
+            ),
+            'daftar_matkul' : forms.CheckboxSelectMultiple()
         }
