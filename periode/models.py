0936e7d71673342d4f52966b437577cb178b43eb
Virdian Harun Prayoga <virdian.harun@ui.ac.id>
2023-05-25 11:12:07 +0700

    Bulan Periode

--- periode/models.py
+++ periode/models.py
@@ -2,7 +2,7 @@ import re
 from django.db import models
 from django.forms import ValidationError
 from accounts.models import TeachingAssistantProfile
-
+from itertools import cycle, islice
 
 def tahun_ajaran_validator(data):
     pattern = r"\d{4}/\d{4}"
@@ -29,13 +29,30 @@ class Periode(models.Model):
         (PENDEK, 'Pendek'),
     ]
 
+    BULAN_CHOICES = [
+        ('JAN','JAN'),
+        ('FEB','FEB'),
+        ('MAR','MAR'),
+        ('APR','APR'),
+        ('MEI','MEI'),
+        ('JUN','JUN'),
+        ('JUL','JUL'),
+        ('AGT','AGT'),
+        ('SEP','SEP'),
+        ('OKT','OKT'),
+        ('NOV','NOV'),
+        ('DES','DES')
+    ]
+
     tahun_ajaran = models.CharField(max_length=9, validators=[tahun_ajaran_validator])
     semester = models.CharField(
-
         max_length=6,
         choices=SEMESTER_CHOICES,
         default=GANJIL,
     )
+
+    bulan_mulai = models.CharField(max_length=3,choices=BULAN_CHOICES, default=BULAN_CHOICES[0])
+    bulan_selesai = models.CharField(max_length=3,choices=BULAN_CHOICES, default=BULAN_CHOICES[4])
 
     daftar_ta = models.ManyToManyField(TeachingAssistantProfile)
 
@@ -45,6 +62,19 @@ class Periode(models.Model):
                 fields=['tahun_ajaran', 'semester'], name='unique_migration_host_combination'
             )
         ]
+
+    def bulan_index_generator(self, index_mulai, index_selesai):
+        yield index_mulai
+        while index_mulai != index_selesai:
+            index_mulai += 1
+            index_mulai %= 12
+            yield index_mulai
+
+    def get_bulan(self):
+        list_bulan = [bulan[0] for bulan in self.BULAN_CHOICES]
+        index_mulai = list_bulan.index(self.bulan_mulai)
+        index_selesai = list_bulan.index(self.bulan_selesai)
+        return [list_bulan[i] for i in self.bulan_index_generator(index_mulai, index_selesai)]
 
     def __str__(self):
         return f"{self.tahun_ajaran} {self.semester}"
